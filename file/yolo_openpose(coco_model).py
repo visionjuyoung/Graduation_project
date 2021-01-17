b32a1b493c81f50895a4246f93576887b8e12e1c
visionjyk21 <visionjyk21@naver.com>
2021-01-18 01:40:35 +0900

    py파일 추가

---
+++ file/yolo_openpose(coco_model).py
@@ -0,0 +1,135 @@
+import cv2 as cv
+import numpy as np
+
+classes = ["person", "bicycle", "car", "motorcycle",
+           "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+           "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+           "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+           "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+           "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+           "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife",
+           "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
+           "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table",
+           "toilet", "tv", "laptop", "mouse", "remote", "keyboard",
+           "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+           "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+                "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+inWidth = 368
+inHeight = 368
+thr = 0.2
+
+cap = cv.VideoCapture(0)
+
+net = cv.dnn.readNetFromTensorflow("C:\\Users\\visio\\folder\\graph_opt.pb")
+YOLO_net = cv.dnn.readNet("C:\\Users\\visio\\folder\\yolov4.weights", "C:\\Users\\visio\\folder\\yolov4.cfg")
+layer_names = YOLO_net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in YOLO_net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+if not cap.isOpened():
+    cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError("cannot open video")
+
+while cv.waitKey(1) < 0:
+    hasFrame, frame = cap.read()
+    h, w, c = frame.shape
+    if not hasFrame:
+        cv.waitKey()
+        break
+
+    blob = cv.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    YOLO_net.setInput(blob)
+    outs = YOLO_net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+
+        for detection in out:
+
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * w)
+                center_y = int(detection[1] * h)
+                dw = int(detection[2] * w)
+                dh = int(detection[3] * h)
+                # Rectangle coordinate
+                x = int(center_x - dw / 2)
+                y = int(center_y - dh / 2)
+                boxes.append([x, y, dw, dh])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            score = confidences[i]
+
+            # 경계상자와 클래스 정보 이미지에 입력
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+            cv.putText(frame, label, (x, y - 20), cv.FONT_ITALIC, 0.5, 
+            (255, 255, 255), 1)
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]
+
+    assert(len(BODY_PARTS) <= out.shape[1])
+
+    points = []
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponding body's part.
+        heatMap = out[0, i, :, :]
+
+        # Originally, we try to find all the local maximums. To simplify a sample
+        # we just find a global one. However only a single pose at the same time
+        # could be detected this way.
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = int(frameWidth * point[0]) / out.shape[3]
+        y = int(frameHeight * point[1]) / out.shape[2]
+
+        # Add a point if it's confidence is higher than threshold.
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert(partFrom in BODY_PARTS)
+        assert(partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
+    t, _ = net.getPerfProfile()
+    freq = cv.getTickFrequency() / 10000
+    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+    
+    cv.imshow('video', frame)
