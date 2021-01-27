e02b3c0979299c81f628aef2a31d428a9401d76a
Juyoung <60837531+visionjuyoung@users.noreply.github.com>
2021-01-27 14:31:23 +0900

    Update yolo_openpose(coco_model).py

--- file/yolo_openpose(coco_model).py
+++ file/yolo_openpose(coco_model).py
@@ -1,5 +1,16 @@
-import cv2 as cv
+import cv2
+import time
 import numpy as np
+from random import randint
+import argparse
+
+parser = argparse.ArgumentParser(description='Run keypoint detection')
+parser.add_argument("--device", default="cpu", help="Device to inference on")
+parser.add_argument("--image_file", default="group2.jpg", help="Input image")
+
+args = parser.parse_args()
+
+cap = cv2.VideoCapture(0)
 
 classes = ["person", "bicycle", "car", "motorcycle",
            "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
@@ -14,42 +25,176 @@ classes = ["person", "bicycle", "car", "motorcycle",
            "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
            "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
-BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-                "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
 
-POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
-
-inWidth = 368
-inHeight = 368
-thr = 0.2
-
-cap = cv.VideoCapture(0)
-
-net = cv.dnn.readNetFromTensorflow("C:\\Users\\visio\\folder\\graph_opt.pb")
-YOLO_net = cv.dnn.readNet("C:\\Users\\visio\\folder\\yolov4.weights", "C:\\Users\\visio\\folder\\yolov4.cfg")
+protoFile = "D:\\lib\\coco\\pose_deploy_linevec.prototxt"
+weightsFile = "D:\\lib\\coco\\pose_iter_440000.caffemodel"
+YOLO_net = cv2.dnn.readNet("D:\\lib\\yolov4.weights", "D:\\lib\\yolov4.cfg")
 layer_names = YOLO_net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in YOLO_net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-if not cap.isOpened():
-    cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    raise IOError("cannot open video")
+nPoints = 18
+# COCO Output Format
+keypointsMapping = ['Nose', 'Neck', 'R-Sho', 'R-Elb', 'R-Wr', 'L-Sho', 'L-Elb', 'L-Wr', 'R-Hip', 'R-Knee', 'R-Ank', 'L-Hip', 'L-Knee', 'L-Ank', 'R-Eye', 'L-Eye', 'R-Ear', 'L-Ear']
 
-while cv.waitKey(1) < 0:
+POSE_PAIRS = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7],
+              [1,8], [8,9], [9,10], [1,11], [11,12], [12,13],
+              [1,0], [0,14], [14,16], [0,15], [15,17],
+              [2,17], [5,16] ]
+
+# index of pafs correspoding to the POSE_PAIRS
+# e.g for POSE_PAIR(1,2), the PAFs are located at indices (31,32) of output, Similarly, (1,5) -> (39,40) and so on.
+mapIdx = [[31,32], [39,40], [33,34], [35,36], [41,42], [43,44],
+          [19,20], [21,22], [23,24], [25,26], [27,28], [29,30],
+          [47,48], [49,50], [53,54], [51,52], [55,56],
+          [37,38], [45,46]]
+
+colors = [ [0,100,255], [0,100,255], [0,255,255], [0,100,255], [0,255,255], [0,100,255],
+         [0,255,0], [255,200,100], [255,0,255], [0,255,0], [255,200,100], [255,0,255],
+         [0,0,255], [255,0,0], [200,200,0], [255,0,0], [200,200,0], [0,0,0]]
+
+
+def getKeypoints(probMap, threshold=0.1):
+
+    mapSmooth = cv2.GaussianBlur(probMap,(3,3),0,0)
+
+    mapMask = np.uint8(mapSmooth>threshold)
+    keypoints = []
+
+    #find the blobs
+    contours, _ = cv2.findContours(mapMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    #for each blob find the maxima
+    for cnt in contours:
+        blobMask = np.zeros(mapMask.shape)
+        blobMask = cv2.fillConvexPoly(blobMask, cnt, 1)
+        maskedProbMap = mapSmooth * blobMask
+        _, maxVal, _, maxLoc = cv2.minMaxLoc(maskedProbMap)
+        keypoints.append(maxLoc + (probMap[maxLoc[1], maxLoc[0]],))
+
+    return keypoints
+
+
+# Find valid connections between the different joints of a all persons present
+def getValidPairs(output):
+    valid_pairs = []
+    invalid_pairs = []
+    n_interp_samples = 10
+    paf_score_th = 0.1
+    conf_th = 0.7
+    # loop for every POSE_PAIR
+    for k in range(len(mapIdx)):
+        # A->B constitute a limb
+        pafA = output[0, mapIdx[k][0], :, :]
+        pafB = output[0, mapIdx[k][1], :, :]
+        pafA = cv2.resize(pafA, (frameWidth, frameHeight))
+        pafB = cv2.resize(pafB, (frameWidth, frameHeight))
+
+        # Find the keypoints for the first and second limb
+        candA = detected_keypoints[POSE_PAIRS[k][0]]
+        candB = detected_keypoints[POSE_PAIRS[k][1]]
+        nA = len(candA)
+        nB = len(candB)
+
+        # If keypoints for the joint-pair is detected
+        # check every joint in candA with every joint in candB
+        # Calculate the distance vector between the two joints
+        # Find the PAF values at a set of interpolated points between the joints
+        # Use the above formula to compute a score to mark the connection valid
+
+        if( nA != 0 and nB != 0):
+            valid_pair = np.zeros((0,3))
+            for i in range(nA):
+                max_j=-1
+                maxScore = -1
+                found = 0
+                for j in range(nB):
+                    # Find d_ij
+                    d_ij = np.subtract(candB[j][:2], candA[i][:2])
+                    norm = np.linalg.norm(d_ij)
+                    if norm:
+                        d_ij = d_ij / norm
+                    else:
+                        continue
+                    # Find p(u)
+                    interp_coord = list(zip(np.linspace(candA[i][0], candB[j][0], num=n_interp_samples),
+                                            np.linspace(candA[i][1], candB[j][1], num=n_interp_samples)))
+                    # Find L(p(u))
+                    paf_interp = []
+                    for k in range(len(interp_coord)):
+                        paf_interp.append([pafA[int(round(interp_coord[k][1])), int(round(interp_coord[k][0]))],
+                                           pafB[int(round(interp_coord[k][1])), int(round(interp_coord[k][0]))] ])
+                    # Find E
+                    paf_scores = np.dot(paf_interp, d_ij)
+                    avg_paf_score = sum(paf_scores)/len(paf_scores)
+
+                    # Check if the connection is valid
+                    # If the fraction of interpolated vectors aligned with PAF is higher then threshold -> Valid Pair
+                    if ( len(np.where(paf_scores > paf_score_th)[0]) / n_interp_samples ) > conf_th :
+                        if avg_paf_score > maxScore:
+                            max_j = j
+                            maxScore = avg_paf_score
+                            found = 1
+                # Append the connection to the list
+                if found:
+                    valid_pair = np.append(valid_pair, [[candA[i][3], candB[max_j][3], maxScore]], axis=0)
+
+            # Append the detected connections to the global list
+            valid_pairs.append(valid_pair)
+        else: # If no keypoints are detected
+            print("No Connection : k = {}".format(k))
+            invalid_pairs.append(k)
+            valid_pairs.append([])
+    return valid_pairs, invalid_pairs
+
+
+
+# This function creates a list of keypoints belonging to each person
+# For each detected valid pair, it assigns the joint(s) to a person
+def getPersonwiseKeypoints(valid_pairs, invalid_pairs):
+    # the last number in each row is the overall score
+    personwiseKeypoints = -1 * np.ones((0, 19))
+
+    for k in range(len(mapIdx)):
+        if k not in invalid_pairs:
+            partAs = valid_pairs[k][:,0]
+            partBs = valid_pairs[k][:,1]
+            indexA, indexB = np.array(POSE_PAIRS[k])
+
+            for i in range(len(valid_pairs[k])):
+                found = 0
+                person_idx = -1
+                for j in range(len(personwiseKeypoints)):
+                    if personwiseKeypoints[j][indexA] == partAs[i]:
+                        person_idx = j
+                        found = 1
+                        break
+
+                if found:
+                    personwiseKeypoints[person_idx][indexB] = partBs[i]
+                    personwiseKeypoints[person_idx][-1] += keypoints_list[partBs[i].astype(int), 2] + valid_pairs[k][i][2]
+
+                # if find no partA in the subset, create a new subset
+                elif not found and k < 17:
+                    row = -1 * np.ones(19)
+                    row[indexA] = partAs[i]
+                    row[indexB] = partBs[i]
+                    # add the keypoint_scores for the two keypoints and the paf_score
+                    row[-1] = sum(keypoints_list[valid_pairs[k][i,:2].astype(int), 2]) + valid_pairs[k][i][2]
+                    personwiseKeypoints = np.vstack([personwiseKeypoints, row])
+    return personwiseKeypoints
+
+
+while cv2.waitKey(1) < 0:
     hasFrame, frame = cap.read()
     h, w, c = frame.shape
     if not hasFrame:
-        cv.waitKey()
+        cv2.waitKey()
         break
 
-    blob = cv.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+     ##################################################### yolo #####################################################################
+
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     YOLO_net.setInput(blob)
     outs = YOLO_net.forward(output_layers)
 
@@ -78,7 +223,8 @@ while cv.waitKey(1) < 0:
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+
 
     for i in range(len(boxes)):
         if i in indexes:
@@ -87,49 +233,72 @@ while cv.waitKey(1) < 0:
             score = confidences[i]
 
             # 경계상자와 클래스 정보 이미지에 입력
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
-            cv.putText(frame, label, (x, y - 20), cv.FONT_ITALIC, 0.5, 
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+            cv2.putText(frame, label, (x, y - 20), cv2.FONT_ITALIC, 0.5, 
             (255, 255, 255), 1)
+
 
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
-    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
-    out = net.forward()
-    out = out[:, :19, :, :]
 
-    assert(len(BODY_PARTS) <= out.shape[1])
+    t = time.time()
+    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+    if args.device == "cpu":
+        net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+        print("Using CPU device")
+    elif args.device == "gpu":
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        print("Using GPU device")
 
-    points = []
-    for i in range(len(BODY_PARTS)):
-        # Slice heatmap of corresponding body's part.
-        heatMap = out[0, i, :, :]
+    # Fix the input Height and get the width according to the Aspect Ratio
+    inHeight = 368
+    inWidth = int((inHeight/frameHeight)*frameWidth)
 
-        # Originally, we try to find all the local maximums. To simplify a sample
-        # we just find a global one. However only a single pose at the same time
-        # could be detected this way.
-        _, conf, _, point = cv.minMaxLoc(heatMap)
-        x = int(frameWidth * point[0]) / out.shape[3]
-        y = int(frameHeight * point[1]) / out.shape[2]
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
+                            (0, 0, 0), swapRB=False, crop=False)
 
-        # Add a point if it's confidence is higher than threshold.
-        points.append((int(x), int(y)) if conf > thr else None)
+    net.setInput(inpBlob)
+    output = net.forward()
+    print("Time Taken in forward pass = {}".format(time.time() - t))
 
-    for pair in POSE_PAIRS:
-        partFrom = pair[0]
-        partTo = pair[1]
-        assert(partFrom in BODY_PARTS)
-        assert(partTo in BODY_PARTS)
+    detected_keypoints = []
+    keypoints_list = np.zeros((0,3))
+    keypoint_id = 0
+    threshold = 0.1
 
-        idFrom = BODY_PARTS[partFrom]
-        idTo = BODY_PARTS[partTo]
+    for part in range(nPoints):
+        probMap = output[0,part,:,:]
+        probMap = cv2.resize(probMap, (frame.shape[1], frame.shape[0]))
+        keypoints = getKeypoints(probMap, threshold)
+        print("Keypoints - {} : {}".format(keypointsMapping[part], keypoints))
+        keypoints_with_id = []
+        for i in range(len(keypoints)):
+            keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+            keypoints_list = np.vstack([keypoints_list, keypoints[i]])
+            keypoint_id += 1
 
-        if points[idFrom] and points[idTo]:
-            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+        detected_keypoints.append(keypoints_with_id)
 
-    t, _ = net.getPerfProfile()
-    freq = cv.getTickFrequency() / 10000
-    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-    
-    cv.imshow('video', frame)
+
+    frameClone = frame.copy()
+    for i in range(nPoints):
+        for j in range(len(detected_keypoints[i])):
+            cv2.circle(frameClone, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+    #cv2.imshow("Keypoints",frameClone)
+
+    valid_pairs, invalid_pairs = getValidPairs(output)
+    personwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs)
+
+    for i in range(17):
+        for n in range(len(personwiseKeypoints)):
+            index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+            if -1 in index:
+                continue
+            B = np.int32(keypoints_list[index.astype(int), 0])
+            A = np.int32(keypoints_list[index.astype(int), 1])
+            cv2.line(frameClone, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+
+
+    cv2.imshow("Detected Pose" , frameClone)
+    cv2.waitKey(0)
