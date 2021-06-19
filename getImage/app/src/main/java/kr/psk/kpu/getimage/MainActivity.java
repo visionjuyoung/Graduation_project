d5c596bda9fd897d67c8e50f113c25bcd78b1ae3
sekyungpark <funnybutterchip@gmail.com>
2021-06-19 22:28:30 +0900

    get images from firbase storage

---
+++ getImage/app/src/main/java/kr/psk/kpu/getimage/MainActivity.java
@@ -0,0 +1,72 @@
+package kr.psk.kpu.getimage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> imagelist;
+    RecyclerView recyclerView;
+    StorageReference root;
+    ProgressBar progressBar;
+    ImageAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imagelist=new ArrayList<>();
+        recyclerView=findViewById(R.id.recyclerview);
+        adapter=new ImageAdapter(imagelist,this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(null));
+        progressBar=findViewById(R.id.progress);
+        progressBar.setVisibility(View.VISIBLE);
+        StorageReference listRef = FirebaseStorage.getInstance().getReference().child("images");
+        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for(StorageReference file:listResult.getItems()){
+                    file.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imagelist.add(uri.toString());
+                            Log.e("Itemvalue",uri.toString());
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            recyclerView.setAdapter(adapter);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+
+}
